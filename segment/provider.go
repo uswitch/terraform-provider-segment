57e2e6de254059e3765cb5b949b0e93be2f20e6b
Andreas Williams <andreas.williams@rvu.co.uk>
2021-03-01 18:02:20 +0000

    Configure provider through env variables or tf config

--- segment/provider.go
+++ segment/provider.go
@@ -1,65 +1,53 @@
 package segment
 
 import (
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"context"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/uswitch/segment-config-go/segment"
 )
 
 // Provider -
 func Provider() *schema.Provider {
 	return &schema.Provider{
-	//   Schema: map[string]*schema.Schema{
-	// 		"accessToken": &schema.Schema{
-	// 		  Type:        schema.TypeString,
-	// 		  Required:    true,
-	// 		  DefaultFunc: schema.EnvDefaultFunc("ACCESS_TOKEN", nil),
-	// 		},
-	// 		"workSpace": &schema.Schema{
-	// 		  Type: 		schema.TypeString,
-	// 		  Required:  	true, 
-	// 		  DefaultFunc:  schema.EnvDefaultFunc("SEGMENT_WORKSPACE", nil),
-	// 		},
-	// 	},
-	  ResourcesMap: map[string]*schema.Resource{
-		  "tracking_plan": resourceTrackingPlan(),  
-	  },
-	  DataSourcesMap: map[string]*schema.Resource{},
-	  ConfigureContextFunc: providerConfigure,
+		Schema: map[string]*schema.Schema{
+			"access_token": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("SEGMENT_ACCESS_TOKEN", nil),
+			},
+			"workspace": &schema.Schema{
+				Type:        schema.TypeString,
+				Required:    true,
+				DefaultFunc: schema.EnvDefaultFunc("SEGMENT_WORKSPACE", nil),
+			},
+		},
+		ResourcesMap: map[string]*schema.Resource{
+			"tracking_plan": resourceTrackingPlan(),
+		},
+		DataSourcesMap:       map[string]*schema.Resource{},
+		ConfigureContextFunc: providerConfigure,
 	}
-  }
+}
 
+func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
+	var diags diag.Diagnostics
 
-  func providerConfigure(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics) {
-
-	  var diags diag.Diagnostics
-	//   accessToken := d.Get("accessToken").(string)
-	  accessToken := "pGXsHj5W6O7D0H_rNOuFQcJNNg9HkV8Puj6WTT2fEag.4fcV8s0zmYaA_MiJICKba0z6Jyzg-x-S2FQGxCxNv8M"
-	//   TODO: use env variable for authorization
-	//   workSpace := d.Get("workSpace").(string)
-	  workSpace := "uswitch-sandbox"
-
-	  if (accessToken != "") && (workSpace != "") {
+	accessToken := d.Get("access_token").(string)
+	workSpace := d.Get("workspace").(string)
+	if accessToken != "" && workSpace != "" {
 		c, err := segment.NewClient(&accessToken, &workSpace)
 		if err != nil {
-		  return nil, diag.FromErr(err)
+			return nil, diag.FromErr(err)
 		}
-	
-		return c, diags
-	  }
-	
-	  c, err := segment.NewClient(nil, nil)
-	  if err != nil {
-		return nil, diag.FromErr(err)
-	  }
-	  return &c, nil
-  }
 
-  
-//   func validateAccessToken(v interface{}, k string) (diag.Diagnostics) {
-// 	  if v == nil || v.(string) == "" {
-// 		  return nil
-// 	  }
-// 	  return nil
-//   }
+		return c, diags
+	}
+
+	c, err := segment.NewClient(nil, nil)
+	if err != nil {
+		return nil, diag.FromErr(err)
+	}
+	return &c, nil
+}
