9bab3fb6563acf60577980f1df397da875ace756
Andreas Williams <andreas.williams@rvu.co.uk>
2021-03-04 12:46:45 +0000

    Add example directory

---
+++ example/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    segment = {
+      source = "uswitch.com/segment/segment"
+      version = "~> 0.0.9"
+    }
+  }
+}
+
+provider "segment" {
+  access_token = "pGXsHj5W6O7D0H_rNOuFQcJNNg9HkV8Puj6WTT2fEag.4fcV8s0zmYaA_MiJICKba0z6Jyzg-x-S2FQGxCxNv8M"
+	workspace = "uswitch-sandbox"
+}
+
+resource "tracking_plan" "test" {
+    provider = segment
+
+    display_name = "AW test tracking plan"
+    rules = file("./rules/test_tracking_plan/rules.json")
+}
