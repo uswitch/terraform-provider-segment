6796bc599d25f2d5ebef049a66c19ea97e9ea492
Andreas Williams <andreas.williams@rvu.co.uk>
2021-03-04 12:46:02 +0000

    Manage tracking plan rules

--- segment/resource_tracking_plan.go
+++ segment/resource_tracking_plan.go
@@ -2,14 +2,8 @@ package segment
 
 import (
 	"context"
+	"encoding/json"
 	"regexp"
-
-	// "encoding/json"
-	// "fmt"
-	"log"
-
-	//   "strconv"
-	//   "time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -35,10 +29,10 @@ func resourceTrackingPlan() *schema.Resource {
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"rules": { //TODO: The rules schema needs more work. an example is provided in the resourceTrackingPlan.txt
-				Type:        schema.TypeString,
-				Optional:    true,
-				Description: "List of identify traits, group traits and events",
+			"rules": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Description:  "List of identify traits, group traits and events",
 				ValidateFunc: validation.StringIsJSON,
 			},
 			"create_time": {
@@ -56,50 +50,82 @@ func resourceTrackingPlan() *schema.Resource {
 }
 
 func resourceTrackingPlanCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-
-	var diags diag.Diagnostics
-	// fmt.Println(reflect.TypeOf(m))
 	client := m.(*segment.Client)
-	
-	log.Println("[INFO] client")
-	log.Println(client)
-	tp := segment.TrackingPlan{
 
+	// unmarshal rules
+	var rules segment.Rules
+	json.Unmarshal([]byte(d.Get("rules").(string)), &rules)
+
+	// construct the tracking plan
+	tp := segment.TrackingPlan{
 		DisplayName: d.Get("display_name").(string),
-		// Rules:       d.Get("rules").(segment.Rules),
-		Rules: d.Get("rules").(string),
+		Rules:       rules,
 	}
-	respText, err := client.CreateTrackingPlan(tp)
+	response, err := client.CreateTrackingPlan(tp)
 	if err != nil {
 		return diag.FromErr(err)
 	}
 
 	// setId shoud utilise the calculated name part in the schema
 	re := regexp.MustCompile(`rs_.*$`)
-	trackingPlanID := re.FindString(respText.Name)
+	trackingPlanID := re.FindString(response.Name)
 	d.SetId(trackingPlanID)
 
-	return diags
-	// TODO: return resourceTrackingPlanRead(ctx, d, m)
+	return resourceTrackingPlanRead(ctx, d, m)
 }
 
-func resourceTrackingPlanUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-
+func resourceTrackingPlanRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	var diags diag.Diagnostics
 	client := m.(*segment.Client)
 
-	tpID := d.Id() 
+	// get a tracking plan by name
+	tp, err := client.GetTrackingPlan(d.Id())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	// set the variables
+	if err := d.Set("name", tp.Name); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := d.Set("display_name", tp.DisplayName); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := d.Set("create_time", tp.CreateTime.String()); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := d.Set("update_time", tp.UpdateTime.String()); err != nil {
+		return diag.FromErr(err)
+	}
+
+	// convert Rules to JSON
+	rulesJSON, err := json.MarshalIndent(tp.Rules, "", "  ")
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	if err := d.Set("rules", string(rulesJSON)); err != nil {
+		return diag.FromErr(err)
+	}
+
+	return diags
+}
+
+func resourceTrackingPlanUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	client := m.(*segment.Client)
+
+	tpID := d.Id()
 	if d.HasChange("display_name") || d.HasChange("rules") {
 		displayName := d.Get("display_name").(string)
-		rules := d.Get("rules").(string)
+		// unmarshal rules
+		var rules segment.Rules
+		json.Unmarshal([]byte(d.Get("rules").(string)), &rules)
 
 		data := segment.TrackingPlan{
 			DisplayName: displayName,
 			Rules:       rules,
 		}
-		tp, err := client.UpdateTrackingPlan(tpID, data)
-
-		log.Println(tp)
+		_, err := client.UpdateTrackingPlan(tpID, data)
 
 		if err != nil {
 			return diag.FromErr(err)
@@ -107,50 +133,18 @@ func resourceTrackingPlanUpdate(ctx context.Context, d *schema.ResourceData, m i
 
 		return resourceTrackingPlanRead(ctx, d, m)
 	}
-
-	//TODO: invoke read to update the state return resourceTrackingPlanRead(ctx, d, m)
-
-	return diags
-}
-
-func resourceTrackingPlanRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	var diags diag.Diagnostics
-	// 	client := m.(*segment.Client)
-
-	// 	// send a get request to get a tracking plan
-	// 	tp, err := client.GetTrackingPlan(d.Id())
-	// 	if err != nil {
-	// 		return diag.FromErr(err)
-	// 	}
-
-	// 	// set the variables
-	// 	if err := d.Set("display_name", tp.DisplayName); err != nil {
-	// 		return diag.FromErr(err)
-	// 	}
-	// 	if err := d.Set("rules", tp.Rules); err != nil {
-	// 		return diag.FromErr(err)
-	// 	}
-	// 	if err := d.Set("create_time", tp.CreateTime); err != nil {
-	// 		return diag.FromErr(err)
-	// 	}
-	// 	if err := d.Set("update_time", tp.UpdateTime); err != nil {
-	// 		return diag.FromErr(err)
-	// 	}
 	return diags
 }
 
 func resourceTrackingPlanDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	var diags diag.Diagnostics
-	log.Println("resourceTrackingplan Delete")
 	client := m.(*segment.Client)
-	id := d.Id()
-	log.Println(id)
+
 	err := client.DeleteTrackingPlan(d.Id())
-		if err != nil {
-			return diag.FromErr(err)
-		}
+	if err != nil {
+		return diag.FromErr(err)
+	}
 
-		d.SetId("")
+	d.SetId("")
 
-	return diags
+	return resourceTrackingPlanRead(ctx, d, m)
 }
