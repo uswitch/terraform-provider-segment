1aff6436f14e45b037c7386d8e81eeba35350da7
Andreas Williams <andreas.williams@rvu.co.uk>
2021-03-18 13:12:02 +0000

    Proposed structure for event libraries

--- example/main.tf
+++ example/main.tf
@@ -12,9 +12,107 @@ provider "segment" {
 	workspace = "uswitch-sandbox"
 }
 
-resource "tracking_plan" "test" {
-    provider = segment
+data "event_library" "generic_events" {
+  event {
+    name = "generic event 1"
+    description = "first generic event"
 
-    display_name = "AW test tracking plan"
-    rules = file("./rules/test_tracking_plan/rules.json")
+    property {
+      type = ""
+      name = ""
+      required = true
+    }
+
+    property {
+      type = ""
+      name = ""
+      required = false
+    }
+  }
+
+  event {
+    name = "generic event 2"
+    description = "second generic event"
+
+    property {
+      type = ""
+      name = ""
+      required = true
+    }
+
+    property {
+      type = ""
+      name = ""
+      required = false
+    }
+  }
+}
+
+resource "tracking_plan" "test" {
+  provider = segment
+
+  display_name = "AW test tracking plan"
+  rules {
+    global {
+      property {
+        type = ""
+        name = ""
+        required = true
+      }
+    }
+
+    event {
+      name = "specific event 1"
+      description = "first specific event"
+
+      property {
+        type = ""
+        name = ""
+        required = true
+      }
+
+      property {
+        type = ""
+        name = ""
+        required = false
+      }
+    }
+
+    event {
+      name = "specific event 2"
+      description = "second specific event"
+
+      property {
+        type = ""
+        name = ""
+        required = true
+      }
+
+      property {
+        type = ""
+        name = ""
+        required = false
+      }
+    }
+
+    identify {
+      property {
+        type = ""
+        name = ""
+        required = true
+      }
+    }
+
+    group {
+      property {
+        type = ""
+        name = ""
+        required = true
+      }
+    }
+  }
+
+
+  rules_source_file = file("./rules/test_tracking_plan/rules.json")  # overrides everything
+  import_from = [data.event_library.generic_events]  # merges together with already defined data either via file or code
 }
