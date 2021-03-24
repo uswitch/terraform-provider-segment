1a77ff1a5c61cacfaf72b3b3d74c4e1760e22f80
Paul Duguet <paul.duguet@rvu.co.uk>
2021-03-24 19:47:21 +0000

    Rename var

--- bin/set-platform.sh
+++ bin/set-platform.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Platform: $PLATFORM"
-PLATFORM=$(echo $PLATFORM | tr '.' '\n')
-export OS=$(echo $PLATFORM | sed '1!d')
-export ARCH=$(echo $PLATFORM | sed '2!d')
+P=$(echo $PLATFORM | tr '.' '\n')
+export OS=$(echo $P | sed '1!d')
+export ARCH=$(echo $P | sed '2!d')
 echo "Building for $OS $ARCH"
