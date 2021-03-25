043b70148151f0039a3aef5f027a88866df37436
Paul Duguet <paul.duguet@rvu.co.uk>
2021-03-25 09:11:15 +0000

    Quote command

--- bin/set-platform.sh
+++ bin/set-platform.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Platform: $PLATFORM"
-P=$(echo $PLATFORM | tr '.' '\n')
+P="$(echo $PLATFORM | tr '.' '\n')"
 export OS=$(echo $P | sed '1!d')
 export ARCH=$(echo $P | sed '2!d')
 echo "Building for $OS $ARCH"
