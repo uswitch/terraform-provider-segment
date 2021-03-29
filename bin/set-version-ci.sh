ea43608d55b51929a4f96e55b322f14cf21bbc94
Paul Duguet <paul.duguet@rvu.co.uk>
2021-03-29 15:37:56 +0100

    fix "

--- bin/set-version-ci.sh
+++ bin/set-version-ci.sh
@@ -1,8 +1,8 @@
 echo "Triggered by $GITHUB_EVENT_NAME"
 version=${GITHUB_SHA}
 if [[ "$GITHUB_EVENT_NAME" == *"release"* ]]; then
-  version=${GITHUB_REF/refs\/tags\//}"
+  version="${GITHUB_REF/refs\/tags\//}"
 fi
 
 echo "Version: $version"
-echo ::set-output name=VERSION::$version
+echo "::set-output name=VERSION::$version"
