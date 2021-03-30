76c73a87136a26e0ebace2014c0f2363468d5867
Paul Duguet <paul.duguet@rvu.co.uk>
2021-03-30 10:59:49 +0100

    fail build

--- main.go
+++ main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
-  "github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 
-  "github.com/uswitch/terraform-provider-segment/segment"
+	"github.com/uswitch/terraform-provider-segment/segment"
 )
 
-
 func main() {
-  plugin.Serve(&plugin.ServeOpts{
-    ProviderFunc: func() *schema.Provider {
-      return segment.Provider()
-    },
-  })
+	foo
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: func() *schema.Provider {
+			return segment.Provider()
+		},
+	})
 }
