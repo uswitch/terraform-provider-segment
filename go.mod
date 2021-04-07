1eb354fd878473b723bbfb045528792a9a2aaa22
Andreas Williams <andreas.williams@rvu.co.uk>
2021-04-07 16:37:09 +0100

    Implement event libraries for tracking plans create resource

--- go.mod
+++ go.mod
@@ -4,4 +4,4 @@ go 1.15
 
 require github.com/hashicorp/terraform-plugin-sdk/v2 v2.2.0
 
-require github.com/uswitch/segment-config-go v0.2.1-0.20210303180935-613aacb035fb
+require github.com/uswitch/segment-config-go v0.2.1-0.20210316161300-bd26d0212910
--- segment/resource_tracking_plan.go
+++ segment/resource_tracking_plan.go
@@ -15,9 +15,9 @@ var client segment.Client
 
 func resourceTrackingPlan() *schema.Resource {
 	return &schema.Resource{
-		CreateContext: resourceTrackingPlanCreate,
+		CreateContext: resourceTrackingPlanCreate, // todo: update to merge json with event library
 		ReadContext:   resourceTrackingPlanRead,
-		UpdateContext: resourceTrackingPlanUpdate,
+		UpdateContext: resourceTrackingPlanUpdate, // todo: update to merge json with event library
 		DeleteContext: resourceTrackingPlanDelete,
 		SchemaVersion: 1,
 		Schema: map[string]*schema.Schema{
@@ -29,11 +29,18 @@ func resourceTrackingPlan() *schema.Resource {
 				Type:     schema.TypeString,
 				Required: true,
 			},
-			"rules": {
+			"rules_json_file": {
 				Type:         schema.TypeString,
 				Optional:     true,
-				Description:  "List of identify traits, group traits and events",
 				ValidateFunc: validation.StringIsJSON,
+			},
+			"import_from": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Elem: &schema.Schema{
+					Type:         schema.TypeString,
+					ValidateFunc: validation.StringIsJSON,
+				},
 			},
 			"create_time": {
 				Type:     schema.TypeString,
@@ -52,9 +59,46 @@ func resourceTrackingPlan() *schema.Resource {
 func resourceTrackingPlanCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	client := m.(*segment.Client)
 
-	// unmarshal rules
-	var rules segment.Rules
-	json.Unmarshal([]byte(d.Get("rules").(string)), &rules)
+	// read rules from json file
+	var rules segment.RuleSet
+	if rulesJSON, ok := d.GetOk("rules_json_file"); ok {
+		err := json.Unmarshal([]byte(rulesJSON.(string)), &rules)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+	}
+
+	// read from event library
+	var eventLibs []EventLibrary
+	if eventLibsIntfcs, ok := d.GetOk("import_from"); ok {
+		var eventLibsList = eventLibsIntfcs.([]interface{})
+		var err error
+		if eventLibs, err = readEventLibs(eventLibsList); err != nil {
+			return diag.FromErr(err)
+		}
+	}
+
+	// flatten event libraries
+	eventLibsFlat := flattenEventLibs(eventLibs)
+
+	// Convert event library events to Segment type events
+	mergedEvents := eventLibsFlat.convertToSegmentEvents()
+
+	// Merge json schema events with ones from the event library.
+	for _, ruleEvnt := range rules.Events {
+		exists := false
+		for i, mergedEvnt := range mergedEvents {
+			if ruleEvnt.Name == mergedEvnt.Name {
+				mergedEvents[i] = ruleEvnt
+				exists = true
+				break
+			}
+		}
+		if !exists {
+			mergedEvents = append(mergedEvents, ruleEvnt)
+		}
+	}
+	rules.Events = mergedEvents
 
 	// construct the tracking plan
 	tp := segment.TrackingPlan{
@@ -78,7 +122,7 @@ func resourceTrackingPlanRead(ctx context.Context, d *schema.ResourceData, m int
 	var diags diag.Diagnostics
 	client := m.(*segment.Client)
 
-	// get a tracking plan by name
+	// get a tracking plan by ID
 	tp, err := client.GetTrackingPlan(d.Id())
 	if err != nil {
 		return diag.FromErr(err)
@@ -103,7 +147,7 @@ func resourceTrackingPlanRead(ctx context.Context, d *schema.ResourceData, m int
 	if err != nil {
 		return diag.FromErr(err)
 	}
-	if err := d.Set("rules", string(rulesJSON)); err != nil {
+	if err := d.Set("rules_json_file", string(rulesJSON)); err != nil {
 		return diag.FromErr(err)
 	}
 
@@ -115,11 +159,11 @@ func resourceTrackingPlanUpdate(ctx context.Context, d *schema.ResourceData, m i
 	client := m.(*segment.Client)
 
 	tpID := d.Id()
-	if d.HasChange("display_name") || d.HasChange("rules") {
+	if d.HasChange("display_name") || d.HasChange("rules_json_file") {
 		displayName := d.Get("display_name").(string)
 		// unmarshal rules
-		var rules segment.Rules
-		json.Unmarshal([]byte(d.Get("rules").(string)), &rules)
+		var rules segment.RuleSet
+		json.Unmarshal([]byte(d.Get("rules_json_file").(string)), &rules)
 
 		data := segment.TrackingPlan{
 			DisplayName: displayName,
@@ -147,4 +191,24 @@ func resourceTrackingPlanDelete(ctx context.Context, d *schema.ResourceData, m i
 	d.SetId("")
 
 	return resourceTrackingPlanRead(ctx, d, m)
+}
+
+func flattenEventLibs(eventLibs []EventLibrary) EventLibrary {
+	var eventLibsFlat EventLibrary
+	for _, evtLib := range eventLibs {
+		eventLibsFlat.Events = append(eventLibsFlat.Events, evtLib.Events...)
+	}
+	return eventLibsFlat
+}
+
+func readEventLibs(eventLibsSlice []interface{}) ([]EventLibrary, error) {
+	var eventLibs []EventLibrary
+	for _, eventLibJSONIntfc := range eventLibsSlice {
+		var eventLib EventLibrary
+		if err := json.Unmarshal([]byte(eventLibJSONIntfc.(string)), &eventLib); err != nil {
+			return nil, err
+		}
+		eventLibs = append(eventLibs, eventLib)
+	}
+	return eventLibs, nil
 }
