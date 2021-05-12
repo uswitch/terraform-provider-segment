9c8079533fc9b9cc6e27ff894685a502603707b9
Andreas Williams <andreas.williams@rvu.co.uk>
2021-05-12 10:36:36 +0100

    Point segment config API client to agouil/segment-config-go

--- segment/resource_source.go
+++ segment/resource_source.go
@@ -7,7 +7,7 @@ import (
 	"reflect"
 	"strings"
 
-	"github.com/ajbosco/segment-config-go/segment"
+	"github.com/agouil/segment-config-go/segment"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
