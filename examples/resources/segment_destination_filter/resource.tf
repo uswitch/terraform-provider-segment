1aed03451090fbd54004c78a23494c1469ad235f
Paul Duguet <paul.duguet@rvu.co.uk>
2021-12-06 16:32:20 +0000

    Destination filters examples

--- examples/resources/segment_destination_filter/resource.tf
+++ examples/resources/segment_destination_filter/resource.tf
@@ -12,6 +12,48 @@ resource "segment_destination_filter" "drop_events" {
 
 # Filter to sample data
 
-# Filter to blacklist fields
+resource "segment_destination_filter" "sample_events" {
+  destination = "test_destination"
+  title       = "Users accepting marketing"
+  description = "Sends 10% of users accepting marketing permissions"
+  condition   = "context.castPermissions.marketing = true"
+  enabled     = true
+  actions {
+    sample {
+      percent = 0.1
+      path    = "userId"
+    }
+  }
+}
 
-# Filter to whitelist fields
+# Filter to block fields
+
+resource "segment_destination_filter" "checkout_block_ip_postcode" {
+  destination = "test_destination"
+  title       = "No IP and postcode on Checkout"
+  description = "Prevents IP address and postcode being sent on checkout"
+  condition   = "event == \"Checkout\""
+  enabled     = true
+  actions {
+    block_fields {
+      context    = ["client.ip"]
+      properties = ["postcode"]
+    }
+  }
+}
+
+# Filter to allow fields
+
+resource "segment_destination_filter" "checkout_allow_price_product" {
+  destination = "test_destination"
+  title       = "Checkout price and product only"
+  description = "Allows only price and product fields to go through on a Checkout event"
+  condition   = "event == \"Checkout\""
+  enabled     = true
+  actions {
+    allow_fields {
+      properties = ["price"]
+      properties = ["product"]
+    }
+  }
+}
